d014df93913f64b4f23d95766f1ac9bbfdcefe9e
baquan1007 <ba.quan1007@gmail.com>
2024-07-03 18:12:02 +0700

    add

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Room } from './room';
+import { RoomList } from './room';
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
@@ -10,6 +11,35 @@ export class RoomsComponent implements OnInit{
   ngOnInit(): void {
     
   }
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms:10,
+    bookedRooms: 5,
+  }
+
+  roomList: RoomList[] =[
+    {
+      roomNumber: 1,
+      roomType: "Delux",
+      amenities:"air",
+      price:500
+
+    },
+    {
+      roomNumber: 2,
+      roomType: "Normal",
+      amenities:"air",
+      price:500
+
+    },
+  ];
   hotelName = "hihi";
-  numberRoom= 10;
+  numberRoom= this.rooms.totalRooms;
+  hideRoom = false
+  
+  toggle() {
+    this.hideRoom = !this.hideRoom;
+  }
+
+  
 }
